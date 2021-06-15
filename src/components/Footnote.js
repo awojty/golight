c0bf6c81126c11c600c7c2f901381c674010c31a
awojty <awojtyna@mit.edu>
2021-06-15 15:42:06 -0400

    attributions

--- src/components/Footnote.js
+++ src/components/Footnote.js
@@ -20,6 +20,10 @@ export default class Footnote extends Component  {
                 <div></div>
                 <div>ul.Adres 1231</div>
                 <div>00-000 Warszawa</div>
+
+                <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                <div>Icons made by <a href="https://www.flaticon.com/authors/dinosoftlabs" title="DinosoftLabs">DinosoftLabs</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
           </div>
         )
 
